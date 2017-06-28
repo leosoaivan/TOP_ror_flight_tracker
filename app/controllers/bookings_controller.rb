8893610f1ca1a43857d3fd54017358185e73ffa6
L. Soai-Van <leo.soaivan@gmail.com>
2017-06-27 23:50:35 -0400

    Add minor edits to views

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,21 +4,25 @@ class BookingsController < ApplicationController
     @booking = Booking.new
     @flight = Flight.find(params[:booking][:flight_id])
     @number = params[:passengers].to_i
+    @departing_air = Airport.find(@flight.departing_id)
+    @arriving_air = Airport.find(@flight.arriving_id)
   end
 
   def create
     @flight = Flight.find(booking_params[:flight_id])
     @booking = @flight.bookings.build(booking_params)
-    
+    permitted_params = booking_params.to_h    
+
     if @booking.save
       redirect_to @booking
     else
       flash.now[:danger] = "#{@booking.errors.messages}"
 
-      permitted_params = booking_params.to_h
       @booking = Booking.new
       @flight = Flight.find(permitted_params[:flight_id])
       @number = permitted_params[:passengers_attributes].count
+      @departing_air = Airport.find(@flight.departing_id)
+      @arriving_air = Airport.find(@flight.arriving_id)
 
       render :new
     end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root 'flights#index'
 
   get 'flights', to: 'flights#index'
-  get 'bookings/new'
+  
+  get 'bookings/new', to: 'bookings#new'
   post 'bookings', to: 'bookings#create'
   get 'bookings/:id', to: 'bookings#show', as: 'booking'
 end
