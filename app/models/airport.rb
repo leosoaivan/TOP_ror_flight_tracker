4b0c0d8570bb2aedfe5902a1f3816c6bb1fb3f74
L. Soai-Van <leo.soaivan@gmail.com>
2017-07-12 13:59:45 -0400

    Add full airport names to menu

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,4 +1,9 @@
 class Airport < ApplicationRecord
   has_many :departures, foreign_key: :departing_id, :class_name => "Flight"
   has_many :arrivals, foreign_key: :arriving_id, :class_name => "Flight"
+
+  def code_with_name
+    "#{code} - #{name} Int'l Airport"
+  end
+  
 end
