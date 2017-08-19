b731a9b46756c35b429c0fe74161c677aec61c56
L. Soai-Van <leo.soaivan@gmail.com>
2017-08-19 12:41:51 -0400

    Edit SendGrid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,11 +78,11 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #SendGrid
-  config.action_mailer.devliery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
+    :user_name => 'apikey',
+    :password => ENV['sendgrid_api_key'],
     :domain => 'yourdomain.com',
     :address => 'smtp.sendgrid.net',
     :post => 587,
